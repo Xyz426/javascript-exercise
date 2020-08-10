b04beced6654f0fa083ccdb10c62a11634706a8b
xyz <xyzzhybz@163.com>
2020-08-11 00:23:58 +0800

    finished array

--- 2-array/find/index.js
+++ 2-array/find/index.js
@@ -1,3 +1,8 @@
 export default function find00OldPerson(collection) {
   // TODO 4: 在这里写实现代码
+  let max = new Date().getFullYear() - 2000;
+  let min = new Date().getFullYear() - 2010;
+
+  //find返回元素，filter返回数组
+  return collection.find(person => person.age <= max && person.age >= min).name;
 }
